9f725f8f4236751a602fdd95c566c618b6b8e1ae
leeyanleryan <leeyanleryan.21@gmail.com>
2023-06-06 00:30:25 +0800

    Minor Change

--- new project/Assets/Scripts/PlayerItems.cs
+++ new project/Assets/Scripts/PlayerItems.cs
@@ -6,7 +6,7 @@ public class PlayerItems : MonoBehaviour, IDataPersistence
 {
     public Inventory inventory;
     public PlayerPositionSO startingPosition;
-    public GameData data;
+    //public GameData data;
     public SpriteRenderer player;
 
     private void Start()
