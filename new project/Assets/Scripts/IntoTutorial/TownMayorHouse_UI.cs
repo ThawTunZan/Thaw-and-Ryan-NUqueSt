1b7ae3c329f34445e8c9d962a0c382664a868b2c
leeyanleryan <leeyanleryan.21@gmail.com>
2023-06-23 12:56:04 +0800

    Secretary addition

--- new project/Assets/Scripts/IntoTutorial/TownMayorHouse_UI.cs
+++ new project/Assets/Scripts/IntoTutorial/TownMayorHouse_UI.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TownMayorHouse_UI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject tutorialPanel;
+    public TextMeshProUGUI tutorialText;
+
+    private bool openTMDialogueDone;
+    public GameObject dialoguePanel;
+
+    private bool progressTMDialogueDone;
+
+    private bool openSecDialogueDone;
+
+    private void Update()
     {
-        
+        if (!openTMDialogueDone)
+        {
+            OpenTMDialogueCheck();
+        }
+        else if (!progressTMDialogueDone)
+        {
+            ProgressTMDialogueCheck();
+        }
+        else if (!openSecDialogueDone)
+        {
+            OpenSecDialogueCheck();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OpenTMDialogueCheck()
     {
-        
+        if (dialoguePanel.activeSelf)
+        {
+            openTMDialogueDone = true;
+            tutorialText.text = "Press SPACEBAR to progress the dialogue";
+        }
+    }
+
+    private void ProgressTMDialogueCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            progressTMDialogueDone = true;
+            tutorialText.text = "";
+            tutorialPanel.SetActive(false);
+        }
+    }
+
+    private void OpenSecDialogueCheck()
+    {
+        if (!dialoguePanel.activeSelf)
+        {
+            tutorialText.text = "Meet the secretary in the living room";
+            tutorialPanel.SetActive(true);
+        }
+        else
+        {
+            openSecDialogueDone = true;
+            tutorialText.text = "";
+            tutorialPanel.SetActive(false);
+        }
     }
 }
