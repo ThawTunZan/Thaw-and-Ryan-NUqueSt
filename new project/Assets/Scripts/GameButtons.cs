d40c1ffea1eec18e127f8dcd42bc366231c7169f
leeyanleryan <leeyanleryan.21@gmail.com>
2023-06-30 03:07:30 +0800

    Changes

--- new project/Assets/Scripts/GameButtons.cs
+++ new project/Assets/Scripts/GameButtons.cs
@@ -36,7 +36,7 @@ public class GameButtons : MonoBehaviour
     public void ContinueGame()
     {
         DisableMenuButtons();
-        SceneManager.LoadSceneAsync("Cave_1");
+        SceneManager.LoadSceneAsync("PlayerHouse");
     }
 
     //disable so that player cant click continue button if there is no saved data
--- new project/Assets/Scripts/PlayerItems.cs
+++ new project/Assets/Scripts/PlayerItems.cs
@@ -21,8 +21,8 @@ public class PlayerItems : MonoBehaviour, IDataPersistence
             toolbar = new Inventory("Toolbar", 7);
             toolbar = GameManager.instance.toolbar;
         }
-        inventory = new Inventory("Inventory", 21);
-        toolbar = new Inventory("Toolbar", 7);
+        //inventory = new Inventory("Inventory", 21);
+        //toolbar = new Inventory("Toolbar", 7);
     }
 
     private void Update()
